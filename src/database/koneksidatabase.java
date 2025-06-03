49dd08f3a0f59a8d04c5d787aad0eab73d1b7d22
makbardarma <peermuints@gmail.com>
2025-06-03 19:06:04 +0700

    koneksi database

--- src/database/koneksidatabase.java
+++ src/database/koneksidatabase.java
@@ -5,11 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class koneksidatabase {
-    private static final String URL = "jdbc:mysql://localhost:3306/administrasi_masyarakat";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
 
-    public static Connection getConnection() throws SQLException {
+    public static void main(String[] args) {
+        try {
+            Connection conn = koneksidatabase.getConnection();
+            if (conn != null) {
+                System.out.println("Koneksi ke database berhasil!");
+                conn.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Koneksi gagal: " + e.getMessage());
+        }
+    }
+
+    private static Connection getConnection() throws SQLException {
+        String URL = "jdbc:mysql://localhost:3306/administrasi_masyarakat";
+        String USER = "root";
+        String PASSWORD = "";
+
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
