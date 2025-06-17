0656451b23140e25a737ffbe0bcabd013d236001
Naufal Charis Zakaria <naufalcharis8@gmail.com>
2025-06-17 22:07:07 +0700

    revisi final

--- src/database/koneksidatabase.java
+++ src/database/koneksidatabase.java
@@ -5,18 +5,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class koneksidatabase {
-    public static void main(String[] args) {
-        try {
-            Connection conn = getConnection();
-            if (conn != null) {
-                System.out.println("Koneksi ke database berhasil!");
-                conn.close(); // Selalu tutup koneksi setelah digunakan
-            }
-        } catch (SQLException e) {
-            System.out.println("Koneksi gagal: " + e.getMessage());
-        }
-    }
-
     public static Connection getConnection() throws SQLException {
         String URL = "jdbc:mysql://localhost:3306/administrasi_mandiri";
         String USER = "root";
